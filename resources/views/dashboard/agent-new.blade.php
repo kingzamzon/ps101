4c458c395a7f48b880d9f0c6137595c074cea632
Adesanoye Samson <adesanoye.samson@yahoo.com>
2020-08-02 04:54:35 +0100

    fixed agent creation based on review

--- resources/views/dashboard/agent-new.blade.php
+++ resources/views/dashboard/agent-new.blade.php
@@ -43,21 +43,22 @@
                 <input type="text" class="form-control" id="first_name" name="first_name" placeholder="Enter your First Name" value="{{ old('first_name') }}" required>
               </div>
               <div class="form-group">
-                <label for="company_name">Company Name</label>
-                <input type="text" class="form-control" id="company_name" name="company_name" placeholder="Enter your Company Name" value="{{ old('company_name') }}" required>
+                <label for="last_name">Last Name</label>
+                <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Enter Last Name" value="{{ old('last_name') }}" required>
               </div>
               <div class="form-group">
-                <label for="tel">Cell phone</label>
-                <input type="tel" class="form-control" id="tel" name="tel" placeholder="Enter your Cell Number" value="{{ old('tel') }}" required>
+                <label for="company_name">Company Name</label>
+                <input type="text" class="form-control" id="company_name" name="company_name" placeholder="Enter your Company Name" value="{{ old('company_name') }}">
               </div>
               <div class="form-group">
                 <label for="tin">Social Security / TIN Number</label>
                 <input type="text" class="form-control" id="tin" name="tin" placeholder="Enter Social Security / TIN Number " value="{{ old('tin') }}" required>
               </div>
               <div class="form-group">
-                <label for="email">Login Email</label>
-                <input type="text" class="form-control" id="email" name="email" placeholder="Enter Username" value="{{ old('email') }}" required>
+                <label for="dob">Date Of Birth</label>
+                <input type="Date" class="form-control" id="dob" name="dob" value="{{ old('dob') }}" required>
               </div>
+
               <div class="form-group">
                 <label for="agent_no">Agent Number</label>
                 <input type="text" class="form-control" id="agent_no" name="agent_no" placeholder="Agent NO" required value="55000" disabled>
@@ -66,46 +67,42 @@
             </div>
             <div class="col-sm-6">
               <div class="form-group">
-                <label for="last_name">Last Name</label>
-                <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Enter Last Name" value="{{ old('last_name') }}" required>
-              </div>
-
-                <div class="row">
-                  <div class="col-sm-4">
-                    <div class="form-group">
-                      <label for="city">City</label>
-                      <input type="text" id="city" class="form-control" placeholder="City" name="city" value="{{ old('city') }}">
-                    </div>
-                  </div>
-                  <div class="col-sm-4">
-                    <div class="form-group">
-                      <label for="state">State</label>
-                      <input type="text" id="state" class="form-control" placeholder="State" name="state" value="{{ old('state') }}">
-                    </div>
-                  </div>
-                  <div class="col-sm-4">
-                    <div class="form-group">
-                      <label for="zip_code">Zip Code</label>
-                      <input type="text" id="zip_code" class="form-control" placeholder="Zip Code" name="zip_code" value="{{ old('zip_code') }}">
-                    </div>
-                  </div>
-                </div>              
-              <div class="form-group">
                 <label for="mail_address">Mailing Address</label>
                 <input type="text" class="form-control" id="mail_address" name="mail_address" placeholder="Mailing Address" value="{{ old('mail_address') }}" required>
               </div>
+              <div class="row">
+                <div class="col-sm-4">
+                  <div class="form-group">
+                    <label for="city">City</label>
+                    <input type="text" id="city" class="form-control" placeholder="City" name="city" value="{{ old('city') }}">
+                  </div>
+                </div>
+                <div class="col-sm-4">
+                  <div class="form-group">
+                    <label for="state">State</label>
+                    <input type="text" id="state" class="form-control" placeholder="State" name="state" value="{{ old('state') }}">
+                  </div>
+                </div>
+                <div class="col-sm-4">
+                  <div class="form-group">
+                    <label for="zip_code">Zip Code</label>
+                    <input type="text" id="zip_code" class="form-control" placeholder="Zip Code" name="zip_code" value="{{ old('zip_code') }}">
+                  </div>
+                </div>
+              </div>              
               <div class="form-group">
-                <label for="home_no">Home Number</label>
-                <input type="tel" class="form-control" id="home_no" name="home_no" placeholder="Enter your Home Number" value="{{ old('home_no') }}">
+                <label for="tel">Cell phone</label>
+                <input type="tel" class="form-control" id="tel" name="tel" placeholder="Enter your Cell Number" value="{{ old('tel') }}" required>
               </div>
               <div class="form-group">
-                <label for="password">Login Password</label>
+                <label for="email">Email</label>
+                <input type="text" class="form-control" id="email" name="email" placeholder="Enter Username" value="{{ old('email') }}" required>
+              </div>
+              <div class="form-group">
+                <label for="password">Password</label>
                 <input type="password" class="form-control" id="password" placeholder="Enter Password" name="password" required>
               </div>
-              <div class="form-group">
-                <label for="dob">Date Of Birth</label>
-                <input type="Date" class="form-control" id="dob" name="dob" value="{{ old('dob') }}" required>
-              </div>
+              
               <!-- col-sm-6  -->
             </div>
           </div>
@@ -129,7 +126,7 @@
               </div>
               <div class="form-group">
                 <label for="routing_no">Routing Number</label>
-                <input type="text" class="form-control" id="routing_no" name="routing_no" placeholder="Routing Number" value="{{ old('routing_no') }}" required>
+                <input type="text" class="form-control" id="routing_no" name="routing_no" placeholder="Routing Number" value="{{ old('routing_no') }}" maxlength="9" required>
               </div>
               <!-- col-sm-6  -->
             </div>
